86d976ba86731d5f99af9c62614cc979e543c3ab
Jonas Windhager <jonas.windhager@uzh.ch>
2023-07-14 15:26:51 +0200

    tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.BASE_URL || '',
+  base: process.env.BASE_URL || "",
   plugins: [react()],
-})
+});

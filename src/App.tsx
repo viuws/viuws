a09682ca6616d10f566780b0a801fd5abf25bc24
Jonas Windhager <jonas.windhager@uzh.ch>
2023-07-14 16:52:09 +0200

    add ReactFlow and TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import ReactFlow, {
+  Background,
+  MiniMap,
+  Controls,
+  useNodesState,
+  useEdgesState,
+} from "reactflow";
+import "reactflow/dist/style.css";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [nodes, _setNodes, onNodesChanged] = useNodesState([]); // eslint-disable-line @typescript-eslint/no-unused-vars
+  const [edges, _setEdges, onEdgesChanged] = useEdgesState([]); // eslint-disable-line @typescript-eslint/no-unused-vars
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChanged}
+        onEdgesChange={onEdgesChanged}
+      >
+        <Background />
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
 
 export default App;

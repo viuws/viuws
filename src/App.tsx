8771575e8999948744fcd8579d9652681f920664
Jonas Windhager <jonas.windhager@uzh.ch>
2023-08-29 17:18:50 +0200

    fix startup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { useEffect } from "react";
 
 import Home from "./pages/Home";
 
+import { Registry } from "./interfaces/registry";
+import { Process } from "./interfaces/process";
+
+import useAppStore from "./stores/appStore";
 import useConfigStore, { Config } from "./stores/configStore";
 
 import { fetchYaml } from "./utils/ioUtils";
@@ -11,14 +15,35 @@ import { fetchYaml } from "./utils/ioUtils";
 function App() {
   const loadConfig = useConfigStore((state) => state.load);
   const configLoaded = useConfigStore((state) => state.loaded);
+  const configRegistries = useConfigStore((state) => state.registries);
+
+  const registerProcess = useAppStore((state) => state.registerProcess);
 
   useEffect(() => {
     async function loadConfigOnStartup() {
       const config = await fetchYaml<Config>("config.yaml");
       loadConfig(config);
     }
-    loadConfigOnStartup().catch(console.error);
-  }, [loadConfig]);
+
+    async function loadRegistryOnStartup(registryUrl: string) {
+      const registry = await fetchYaml<Registry>(registryUrl);
+      if (registry.processes) {
+        for (const processId in registry.processes) {
+          const processPath = registry.processes[processId];
+          const processUrl = registryUrl + "/" + processPath;
+          fetchYaml<Process>(processUrl).then(registerProcess, console.error);
+        }
+      }
+    }
+
+    if (!configLoaded) {
+      loadConfigOnStartup().then(() => {
+        for (const registryUrl of configRegistries) {
+          loadRegistryOnStartup(registryUrl).catch(console.error);
+        }
+      }, console.error);
+    }
+  }, [loadConfig, configLoaded, configRegistries, registerProcess]);
 
   if (!configLoaded) {
     return (
--- src/utils/ioUtils.ts
+++ src/utils/ioUtils.ts
@@ -1,10 +1,5 @@
 import { load as loadYaml } from "js-yaml";
 
-import { Process } from "../interfaces/process";
-import { Registry } from "../interfaces/registry";
-
-import useAppStore from "../stores/appStore";
-
 export async function fetchYaml<T>(url: string) {
   const response = await window.fetch(url);
   if (response.ok) {
@@ -12,25 +7,5 @@ export async function fetchYaml<T>(url: string) {
     return loadYaml(text) as T;
   } else {
     return Promise.reject(response.statusText);
-  }
-}
-
-async function loadRegistryProcesses(registryUrl: string, registry: Registry) {
-  const registerProcess = useAppStore((state) => state.registerProcess);
-  for (const processId in registry.processes) {
-    if (registry.processes.hasOwnProperty(processId)) {
-      const processPath = registry.processes[processId];
-      fetchYaml<Process>(registryUrl + "/" + processPath).then(registerProcess);
-    }
-  }
-}
-
-export async function loadRegistry(
-  registryUrl: string,
-  registryPath: string = "/.viuws-registry.yaml",
-) {
-  const registry = await fetchYaml<Registry>(registryUrl + registryPath);
-  if (registry.processes) {
-    loadRegistryProcesses(registryUrl, registry);
   }
 }

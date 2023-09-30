9a85001446570ea00754ba9d1e84011698fe34a2
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-09-30 17:21:41 +0200

    minor changes

--- generate-interfaces.sh
+++ generate-interfaces.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 rm -rf src/interfaces
+mkdir -p src/interfaces
 for file in "$@"; do
     npx json2ts "$file" "src/interfaces/$(basename "$file" .json).ts"
 done
-npx prettier src/interfaces --write
+npx prettier --write "src/interfaces"
